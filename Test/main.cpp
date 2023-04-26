4441d78f1edc225477ea88b0c5ba5594d7cf336c
Konstantin Koziura <kozyura0111@gmail.com>
2023-04-26 13:08:21 +0300

    Test proj

---
+++ Test/main.cpp
@@ -0,0 +1,13 @@
+extern "C"
+{
+    #include<linux/i2c-dev.h>
+}
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
